b43e6bdd7516a103921500141ff2991ecf002143
Andreas Pfurtscheller <1051396+aplr@users.noreply.github.com>
2024-07-09 12:35:13 +0100

    make crypt.Capsule database serializable

--- core/crypt/models.go
+++ core/crypt/models.go
@@ -1,7 +1,33 @@
 package crypt
 
+import (
+	"database/sql"
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
 type Capsule struct {
 	Data       []byte
 	KeyName    string
 	KeyVersion int
+}
+
+var _ = driver.Valuer(&Capsule{})
+
+// Value implements the driver.Valuer interface
+func (c Capsule) Value() (driver.Value, error) {
+	return json.Marshal(c)
+}
+
+var _ = sql.Scanner(&Capsule{})
+
+// Scan implements the sql.Scanner interface
+func (c *Capsule) Scan(value interface{}) error {
+	str, ok := value.(string)
+	if !ok {
+		return errors.New("type assertion to string failed")
+	}
+
+	return json.Unmarshal([]byte(str), c)
 }
