c715be6c51ee9fcab11598239ec1e1ee977eaf21
Andreas Pfurtscheller <1051396+aplr@users.noreply.github.com>
2024-07-22 17:32:21 +0100

    fix import cycle

--- core/crypt/key_provider_vault.go
+++ core/crypt/key_provider_vault.go
@@ -3,7 +3,6 @@ package crypt
 import (
 	"context"
 
-	"github.com/fruitsco/goji/core/crypt"
 	"github.com/fruitsco/goji/core/vault"
 	"go.uber.org/fx"
 )
@@ -18,34 +17,34 @@ type VaultKeyProviderParams struct {
 	Vault *vault.Vault
 }
 
-func NewVaultKeyProvider(params VaultKeyProviderParams) crypt.KeyProvider {
+func NewVaultKeyProvider(params VaultKeyProviderParams) KeyProvider {
 	return &VaultKeyProvider{
 		vault: params.Vault,
 	}
 }
 
-var _ = crypt.KeyProvider(&VaultKeyProvider{})
+var _ = KeyProvider(&VaultKeyProvider{})
 
-func (v *VaultKeyProvider) GetKey(ctx context.Context, name string) (crypt.Key, error) {
+func (v *VaultKeyProvider) GetKey(ctx context.Context, name string) (Key, error) {
 	secret, err := v.vault.GetLatestVersion(ctx, name)
 	if err != nil {
-		return crypt.Key{}, err
+		return Key{}, err
 	}
 
-	return crypt.Key{
+	return Key{
 		Name:    secret.Name,
 		Version: secret.Version,
 		Data:    secret.Payload,
 	}, nil
 }
 
-func (v *VaultKeyProvider) GetKeyVersion(ctx context.Context, name string, version int) (crypt.Key, error) {
+func (v *VaultKeyProvider) GetKeyVersion(ctx context.Context, name string, version int) (Key, error) {
 	secret, err := v.vault.GetVersion(ctx, name, version)
 	if err != nil {
-		return crypt.Key{}, err
+		return Key{}, err
 	}
 
-	return crypt.Key{
+	return Key{
 		Name:    secret.Name,
 		Version: secret.Version,
 		Data:    secret.Payload,
